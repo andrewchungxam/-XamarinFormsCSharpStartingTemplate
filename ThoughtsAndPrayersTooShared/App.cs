2e64a6b7284b01d48b74855d17efdfe54578eb46
ac <andrew.chung@xamarin.com>
2017-10-24 11:16:39 -0400

    Added Template
    Added Template

---
+++ ThoughtsAndPrayersTooShared/App.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using XamarinFormsTemplateCSharp.Resources;
+using XamarinFormsTemplateCSharp.Views;
+using XamarinFormsTemplateCSharp.Services;
+using Xamarin.Forms;
+
+namespace XamarinFormsTemplateCSharp
+{
+	public class App : Application
+	{
+		public static bool UseMockDataStore = true;
+		public static string BackendUrl = "https://localhost:5000";
+
+		public App()
+		{
+            //DATA SOURCES
+			if (UseMockDataStore)
+				DependencyService.Register<MockDataStore>();
+			else
+				DependencyService.Register<CloudDataStore>();
+
+			if (Device.RuntimePlatform == Device.iOS)
+				MainPage = new MainPage();
+			else
+				MainPage = new NavigationPage(new MainPage());
+
+			//ADDING STYLES TO RESOURCE DICTIONARIES
+			Resources = new ResourceDictionary();
+			//IMPLICIT 
+			Resources.Add(StyleDictionary.navigationPageStyle);
+			//EXPLICIT
+			//Resources.Add("navigationPageStyle", StyleDictionary.navigationPageStyle );
+
+		}
+	}
+}
